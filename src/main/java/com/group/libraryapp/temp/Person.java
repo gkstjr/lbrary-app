fb7ef497240f3a7b483d4b703142a42f998dcb23
김한석 <gkstjr8332@naver.com>
2023-02-22 22:54:50 +0900

    첫 번쨰 커밋

---
+++ src/main/java/com/group/libraryapp/temp/Person.java
@@ -0,0 +1,15 @@
+package com.group.libraryapp.temp;
+
+import javax.persistence.*;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+
+    private String name;
+
+    @OneToOne
+    private Address address;
+}
